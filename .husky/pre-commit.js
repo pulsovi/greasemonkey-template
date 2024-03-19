8d4e49d65a7d79f468d11379484d723176a08b57
David GABISON <david.gabison@outlook.com>
2024-03-19 12:07:00 +0100

    feat: add pre-commit content

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+import fs from 'node:fs';
+const script = (await fs.promises.readdir('.')).find(item => item.endsWith('.user.js'));
+const content = await fs.promises.readFile(script, 'utf8');
+const lines = content.split('\n');
+const meta = lines.slice(0, lines.indexOf('// ==/UserScript==') + 1).join('\n');
+const metaFile = script.replace(/.user.js$/u, '.meta.js');
+await fs.promises.writeFile(metaFile, meta, 'utf8');
